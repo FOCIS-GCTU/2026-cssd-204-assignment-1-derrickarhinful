de4ede468dd83fe27cc5c3ca442e706b65dd0778
derrickarhinful <arhinfulderrick14@gmail.com>
2026-06-02 12:54:14 +0000

    assignmentt

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Derrick Kwesi Mensah Arhinful
+# STUDENT ID: 2425403075
+# Email: arhinfulderrick14@gmail.com
+# Grader: Augustus
+#
+# On my honor, Derrick Kwesi Mensah Arhinful, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+
+    print()
+    print("...DKA")
+    print()
+    print("...DDDDDDDD............K........k.............AAAAAA........")
+    print("...DDDDDDDDDD..........K.......k............AAAAAAAAAA......")
+    print("...DD......DDD.........K......k............AAA......AAA.....")
+    print("...DD.......DD.........K....k..............AA........AA.....")
+    print("...DD.......DD.........K...k...............A..........A.....")
+    print("...DD.......DD.........K.k.................AAAAAAAAAAAA.....")
+    print("...DD.......DD.........K....K..............AAAAAAAAAAAA.....")
+    print("...DD......DDD.........K.....K.............A..........A.....")
+    print("...DDDDDDDDDD.....**...K.......K......**...AA........AA...**")
+    print("...DDDDDDDD.......**...K........K.....**...AA........AA...**")
+    print()
 
 
-
-main()
+if __name__ == "__main__":
+    main()
